c36799654ec865d55a45e66d05e62dd889c2a470
Matthias Nielsen <mscn@itu.dk>
2025-09-18 17:44:24 +0200

    Can send from client to server now
    Call "go run server.go" in one terminal and "go run client.go" in another for it to work

--- TCP Simulator/client.go
+++ TCP Simulator/client.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"encoding/binary"
+	"net"
+)
+
+func main() {
+	connection, err := net.Dial("tcp", "localhost:6969")
+
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close() //If shit hits the fan, part 2: Electric bogaloo
+
+	seq := make([]byte, 4)
+	binary.BigEndian.PutUint32(seq, 300)
+	connection.Write([]byte{1, 2, 3, 4, 5})
+}
--- TCP Simulator/server.go
+++ TCP Simulator/server.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+type TCP struct {
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:6969")
+
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close() //If shit hits the fan
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		readBuffer := make([]byte, 4) //Size 4 since that's what TCP uses.
+		bytesReceived, _ := connection.Read(readBuffer)
+		if bytesReceived != 4 {
+			fmt.Println("Expected 4 bytes, received", bytesReceived)
+			continue
+		}
+		fmt.Println(binary.BigEndian.Uint32(readBuffer))
+	}
+}
