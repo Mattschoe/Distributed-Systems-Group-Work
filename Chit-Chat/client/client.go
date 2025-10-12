6c4a53ca41c625ea335aaabe61636204e0ea10f3
Lukas <lukasshaghashvili14@gmail.com>
2025-10-12 22:10:59 +0200

    feat: add chit-chat code

---
+++ Chit-Chat/client/client.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	proto "chit-chat/grpc"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+
+	var username string
+	vectorClock := &proto.VectorClocks{
+		Clocks: make(map[string]int64),
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	conn, err := grpc.NewClient("localhost:8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Connection failed: %v", err)
+	}
+	defer conn.Close()
+
+	client := proto.NewChitChatClient(conn)
+
+	fmt.Print("Type your username: ")
+	fmt.Scan(&username)
+
+	stream, err := client.ReceiveMessage(context.Background(), &proto.JoinRequest{User: username})
+	if err != nil {
+		log.Fatalf("Failed to join chat: %v", err)
+	}
+
+	go func() {
+		for {
+			chatMessage, err := stream.Recv()
+			if err != nil {
+				log.Println("Disconnected from chat:", err)
+				return
+			}
+
+			if chatMessage.VectorClocks != nil {
+				safeIncrement(vectorClock, username)
+				for user, theirClocks := range chatMessage.VectorClocks.Clocks {
+					myClock := vectorClock.Clocks[user]
+					vectorClock.Clocks[user] = max(theirClocks, myClock)
+				}
+
+			}
+
+			log.Println(chatMessage.User, ":", chatMessage.Message)
+		}
+	}()
+
+	fmt.Println("type your message: ")
+	for {
+		scanner.Scan()
+		message := scanner.Text()
+		if message == ".quit" {
+			break
+		}
+		safeIncrement(vectorClock, username)
+		client.SendMessage(context.Background(),
+			&proto.SendMessageRequest{User: username,
+				Message:      message,
+				VectorClocks: vectorClock})
+		time.Sleep(2 * time.Second)
+	}
+
+	time.Sleep(10 * time.Second)
+}
+func safeIncrement(vc *proto.VectorClocks, user string) {
+	if vc.Clocks == nil {
+		vc.Clocks = make(map[string]int64)
+	}
+	vc.Clocks[user]++
+}
---
+++ Chit-Chat/server/server.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	proto "chit-chat/grpc"
+	"context"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type grpcServer struct {
+	proto.UnimplementedChitChatServer
+	clients        map[string]proto.ChitChat_ReceiveMessageServer
+	mutex          sync.RWMutex
+	messageHistory []*proto.ChatMessage
+}
+
+func (s *grpcServer) GetTime(ctx context.Context, in *proto.Empty) (*proto.TimeOld, error) {
+	time2 := time.Now().UnixNano()
+	time.Sleep(100 * time.Millisecond)
+	time3 := time.Now().UnixNano()
+	return &proto.TimeOld{Time2: time2, Time3: time3}, nil
+}
+
+func (s *grpcServer) ReceiveMessage(req *proto.JoinRequest, stream proto.ChitChat_ReceiveMessageServer) error {
+	user := req.User
+
+	s.broadcastJoinMessage(user)
+	s.addClient(user, stream)
+
+	<-stream.Context().Done()
+
+	s.removeClient(user)
+	s.broadcastLeaveMessage(user)
+
+	return nil
+}
+
+func (s *grpcServer) addClient(user string, stream proto.ChitChat_ReceiveMessageServer) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.clients[user] = stream
+	log.Printf("created new client %s", user)
+	return nil
+}
+
+func (s *grpcServer) removeClient(user string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete(s.clients, user)
+	log.Printf("deleted %s", user)
+	return nil
+}
+
+func (s *grpcServer) broadcastMessage(message *proto.ChatMessage) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	if message.VectorClocks != nil && message.VectorClocks.Clocks != nil {
+		for user := range message.VectorClocks.Clocks {
+			log.Printf("-----------")
+			log.Printf("user %s -> %d clockCount", user, message.VectorClocks.Clocks[user])
+			log.Printf("-----------")
+		}
+	}
+
+	for user, stream := range s.clients {
+		if err := stream.Send(message); err != nil {
+			go func(user string) {
+				s.removeClient(user)
+			}(user)
+		}
+	}
+}
+
+func (s *grpcServer) SendMessage(ctx context.Context, in *proto.SendMessageRequest) (*proto.Time, error) {
+	var message = &proto.ChatMessage{User: in.User,
+		Message:      in.Message,
+		Type:         proto.ChatMessage_REGULAR,
+		VectorClocks: in.VectorClocks,
+	}
+
+	s.messageHistory = append(s.messageHistory, message)
+
+	s.broadcastMessage(message)
+
+	return &proto.Time{Time: time.Now().UnixNano()}, nil
+}
+
+func (s *grpcServer) broadcastJoinMessage(user string) {
+	message := &proto.ChatMessage{User: user,
+		Message: "-- joins the chat --",
+		Type:    proto.ChatMessage_JOIN,
+		VectorClocks: &proto.VectorClocks{
+			Clocks: make(map[string]int64),
+		},
+	}
+
+	s.messageHistory = append(s.messageHistory, message)
+
+	s.broadcastMessage(message)
+}
+
+func (s *grpcServer) broadcastLeaveMessage(user string) {
+
+	s.messageHistory = append(s.messageHistory, &proto.ChatMessage{User: user,
+		Message: "-- left the chat --",
+		Type:    proto.ChatMessage_LEAVE,
+	})
+}
+
+func main() {
+	server := &grpcServer{clients: map[string]proto.ChitChat_ReceiveMessageServer{}}
+
+	server.start_server()
+}
+
+func (s *grpcServer) start_server() {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatalf("no")
+	}
+
+	proto.RegisterChitChatServer(grpcServer, s)
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("no")
+	}
+
+}
